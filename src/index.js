56d58f3731d7d1b0b2732f168a36fe5581bacd98
TristanAG <mac412@Mac412.ad.nordicnaturals.com>
2018-02-08 16:01:31 -0800

    turning into components

--- src/index.js
+++ src/index.js
@@ -1,23 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/normalize.css';
-import './css/skeleton.css';
-import './css/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './css/normalize.css'
+import './css/skeleton.css'
+import './css/index.css'
 // import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker'
 
 const app = {
-  title: 'todo and chill'
+  title: 'todo and chill',
+  todos: []
 }
 
-const template = (
-  <div className="container" style={{marginTop: '15px'}}>
-    <header>
-      <h5>{app.title}</h5>
-    </header>
-    <p>hello</p>
-  </div>
+
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div className="container" style={{marginTop: '15px'}}>
+        <Header />
+        <Prompt />
+        <TodoList />
+        <AddTodo />
+
+
+        {
+          app.todos.length > 0 && <span className="clear-all" onClick={clearTodos}>clear all</span>
+        }
+      </div>
+    )
+  }
+}
+
+const addTodo = (e) => {
+  e.preventDefault()
+  const todo = e.target.elements.todo.value
+  app.todos.push(todo)
+  e.target.elements.todo.value = ''
+  render()
+}
+
+const clearTodos = () => {
+  console.log('hello')
+  app.todos = []
+  render()
+}
+
+const Header = () => (
+  <header>
+    <h5>{app.title}</h5>
+  </header>
 )
 
-ReactDOM.render(template, document.getElementById('root'));
-registerServiceWorker();
+const Prompt = () => (
+  <p>
+    {
+      app.todos.length > 0 ?
+        `you have ${app.todos.length} todos` :
+        'add some todos, won\'t you?'
+    }
+  </p>
+)
+
+const TodoList = () => (
+  <ul>
+    {
+      app.todos.map((todo) => {
+        return <li key={todo}>{todo}</li>
+      })
+    }
+  </ul>
+)
+
+class AddTodo extends React.Component {
+  render() {
+    return (
+      <form onSubmit={addTodo}>
+        <input type="text" name="todo" />
+        <button>+</button>
+      </form>
+    )
+  }
+}
+
+const render = () => {
+  ReactDOM.render(<TodoApp />, document.getElementById('root'));
+}
+
+render()
+// registerServiceWorker();
