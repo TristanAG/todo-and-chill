b9b579eba4b308a6f8dba91a7c495cd74cc1fcf1
TristanAG <tristan.gruener@gmail.com>
2018-02-11 15:28:59 -0800

    able to remove individual todos and style

--- src/index.js
+++ src/index.js
@@ -11,18 +11,17 @@ class TodoApp extends React.Component {
     super(props)
     this.handleClearAll = this.handleClearAll.bind(this)
     this.handleAddTodo = this.handleAddTodo.bind(this)
+    this.handleDeleteTodo = this.handleDeleteTodo.bind(this)
     this.state = {
-      todos: ['buy crystals', 'meditate + yoga', 'go to 6th dimension'],
+      todos: ['buy crystals 💎', 'meditate + yoga 🌴', 'go to 6th dimension 🔮'],
       numberOfTodos: 3
     }
   }
   handleClearAll() {
-    this.setState(() => {
-      return {
-        todos: [],
-        numberOfTodos: 0
-      }
-    })
+    this.setState(() => ({
+      todos: [],
+      numberOfTodos: 0
+    }))
   }
   handleAddTodo(todo) {
     if (!todo) {
@@ -31,12 +30,19 @@ class TodoApp extends React.Component {
       return 'You already entered this... ambitions much?'
     }
 
-    this.setState((prevState) => {
-      return {
-        todos: prevState.todos.concat(todo),
-        numberOfTodos: prevState.todos.length + 1
-      }
-    })
+    this.setState((prevState) => ({
+      todos: prevState.todos.concat(todo),
+      numberOfTodos: prevState.todos.length + 1
+    }))
+  }
+  handleDeleteTodo(todoToDelete) {
+    this.setState((prevState) => ({
+      todos: prevState.todos.filter((todo) => {
+        return todoToDelete !== todo
+      }),
+      numberOfTodos: prevState.numberOfTodos - 1
+    }))
+
   }
 
   render() {
@@ -45,7 +51,7 @@ class TodoApp extends React.Component {
       <div className="container" style={{marginTop: '15px'}}>
         <Header title={title}/>
         <Prompt numberOfTodos={this.state.numberOfTodos}/>
-        <TodoList todos={this.state.todos}/>
+        <TodoList todos={this.state.todos} handleDeleteTodo={this.handleDeleteTodo}/>
         <AddTodo handleAddTodo={this.handleAddTodo}/>
         <ClearTodos
           hasTodos={this.state.numberOfTodos > 0}
@@ -56,24 +62,21 @@ class TodoApp extends React.Component {
   }
 }
 
-const Header = ({ title }) => {
-  return (
-    <header>
-      <h5>{title}</h5>
-    </header>
-  )
-}
-
+const Header = ({ title }) => (
+  <header>
+    <h5>{title}</h5>
+  </header>
+)
 
 const Prompt = ({ numberOfTodos }) => {
-  let prompt = ''
+  // let prompt = ''
 
   if(numberOfTodos === 0){
-    prompt = 'add some todos, won\'t you?'
+    return <p><i>add some todos, won't you?</i></p>
   } else if (numberOfTodos === 1){
-    prompt = `you have ${numberOfTodos} todo`
+    return <p><i>you have <b>1</b> todo</i></p>
   } else {
-    prompt =  `you have ${numberOfTodos} todos`
+    return <p><i>you have <b>{numberOfTodos}</b> todos</i></p>
   }
 
   return (
@@ -81,15 +84,72 @@ const Prompt = ({ numberOfTodos }) => {
   )
 }
 
-const TodoList = ({ todos }) => (
-  <ul>
-    {todos.map((todo) => <Todo key={todo} todoText={todo}/>)}
-  </ul>
+
+// const Prompt = ({ numberOfTodos }) => {
+//   let prompt = ''
+//
+//   if(numberOfTodos === 0){
+//     prompt = 'add some todos, won\'t you?'
+//   } else if (numberOfTodos === 1){
+//     prompt = `you have ${numberOfTodos} todo`
+//   } else {
+//     prompt = `you have ${numberOfTodos} todos`
+//   }
+//
+//   return (
+//     <p>{prompt}</p>
+//   )
+// }
+
+const TodoList = (props) => (
+  <div>
+    {props.todos.map((todo, index) => (
+      <Todo
+        key={todo}
+        todoText={todo}
+        id={index}
+        handleDeleteTodo={props.handleDeleteTodo}
+      />
+    ))}
+  </div>
 )
 
+const Todo = ({ todoText, handleDeleteTodo }) => (
+  <div className="todo">
+    {todoText}
+       <span
+        className="delete-todo"
+        onClick={() => {
+          handleDeleteTodo(todoText)
+        }}
+      >
+        [<b>x</b>]
+      </span>
+    </div>
+)
 
-const Todo = ({ todoText }) => <li>{todoText}</li>
-
+//
+// class Todo extends React.Component {
+//   constructor(props) {
+//     super(props)
+//
+//   }
+//   render(){
+//     return (
+//       <div className="todo">
+//         {this.props.todoText}
+//         <span
+//           className="delete-todo"
+//           onClick={() => {
+//             this.props.handleDeleteTodo(this.props.todoText)
+//           }}
+//         >
+//           [<b>x</b>]
+//         </span>
+//       </div>
+//     )
+//   }
+// }
 
 class AddTodo extends React.Component {
   constructor(props) {
@@ -103,11 +163,8 @@ class AddTodo extends React.Component {
     e.preventDefault()
     const todo = e.target.elements.todo.value.trim()
     const error = this.props.handleAddTodo(todo)
-    //if nothing comes back (i.e. an error) that means it worked
-    this.setState(() => {
-      return { error }
-    })
 
+    this.setState(() => ( {error} ))
     e.target.elements.todo.value = ''
   }
 
@@ -117,7 +174,7 @@ class AddTodo extends React.Component {
         {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.addTodo}>
           <input type="text" name="todo" />
-          <button>+</button>
+          <button>add todo</button>
         </form>
       </div>
     )
@@ -131,6 +188,8 @@ const ClearTodos = ({ hasTodos, handleClearAll }) => {
   return null
 }
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(
+  <TodoApp />,
+document.getElementById('root'));
 
 // registerServiceWorker();
